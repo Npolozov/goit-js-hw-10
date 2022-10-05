2a3db2d556b807dd3b64e556f6fd8cc699e61137
Nikita Polozov <npolozov0@gmail.com>
2022-10-05 15:28:15 +0300

    !

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
+const DEBOUNCE_DELAY = 300;
+
+fetch(`https://restcountries.com/v3.1/name/peru`);

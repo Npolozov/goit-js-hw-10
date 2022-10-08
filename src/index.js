4cf574bb91bcccc7a449f42770a29818ce0dd81b
Nikita Polozov <npolozov0@gmail.com>
2022-10-08 22:02:17 +0300

    !

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
 import './css/styles.css';
+import { refs } from './js/refs';
+import {createMurkUp} from './js/createMarkup';
+import {countryMurkUp} from './js/createMarkup';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { UnsplashAPI } from './js/UnsplashAPI';
 import debounce from 'lodash.debounce';
 
+const unsplash = new UnsplashAPI();
 const DEBOUNCE_DELAY = 300;
 
-fetch(`https://restcountries.com/v3.1/name/peru`);
+refs.input.addEventListener('input', debounce(searchInput, DEBOUNCE_DELAY));
+
+  function searchInput(event) {
+    event.preventDefault();
+    const name = event.target.value.trim().toLowerCase();
+    if (name === '') {
+      return (refs.countryWrapperRef.innerHTML =''), (refs.countryInfo.innerHTML ='')
+    }
+    unsplash.getCountrybyName(name).then(data => {
+      refs.countryWrapperRef.innerHTML ='';
+      refs.countryInfo.innerHTML ='';
+      if (name.length) {
+        alertTooManyMatches()
+      }
+      else if(data.length >= 2 && data.length <= 10) {
+        const murkup = createMurkUp(data);
+        refs.countryWrapperRef.insertAdjacentHTML('beforeend', murkup);
+      } else if (data.length === 1) {
+        refs.countryWrapperRef.innerHTML ='';
+        const murkupInfo = countryMurkUp(data);
+        refs.countryInfo.insertAdjacentHTML('beforeend', murkupInfo);
+      } 
+    }).catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+    });
+  }
+
+
+  function alertTooManyMatches() {
+    Notify.info('Too many matches found. Please enter a more specific name.')
+  }
+
+  
---
+++ src/js/UnsplashAPI.js
@@ -0,0 +1,12 @@
+export class UnsplashAPI {
+    getCountrybyName (name) {
+      const url = `https://restcountries.com/v3.1/name/${name}`
+        return fetch(url)  
+        .then(response => {
+            if (!response.ok) {
+              throw new Error(response.status);
+            }
+            return response.json();
+          })
+      }
+}
